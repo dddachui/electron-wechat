32d52f01060507df0be77347a56e9e0bf61a934e
wangsuyu <1395168068@qq.com>
2023-12-03 21:53:42 +0800

    优化

--- install.sh
+++ install.sh
@@ -5,13 +5,14 @@ if [[ $(id -u) -ne 0 ]]; then
         exit
 fi
 if [ ! -f weixin-$version.tar.gz ]; then
+        echo -e "\033[31;1m 找不到weixin-$version.tar.gz \033[0m"
         exit
 else
         if ! md5sum -c packages.md5 >/dev/null; then
-                echo -e "\033[31;1m校验文件失败,请检查文件完整性\033[0m"
+                echo -e "\033[31;1m 校验文件失败,请检查文件完整性 \033[0m"
                 exit
         else
-                tar -xvf weixin-$version.tar.gz -C /opt
+                tar -xf weixin-$version.tar.gz -C /opt
                 ln -sf /opt/weixin/weixin /usr/bin/weixin
                 /usr/bin/cp lsb-release lsb-release-openkylin /etc
                 /usr/bin/cp libactivation.so /usr/lib/
@@ -20,5 +21,6 @@ else
                 chmod 4755 /opt/weixin/chrome-sandbox
                 /usr/bin/cp -r share/ /usr/
                 /usr/bin/cp weixin.desktop /usr/share/applications/
+                echo -e "\033[32;1m 安装完成,使用方法请看readme.md \033[0m"
         fi
 fi
